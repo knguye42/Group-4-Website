f72bd96517315d2e37be2f552dbaaef58bb05a0c
Makhi Austin <40926690+makhia@users.noreply.github.com>
2020-10-09 12:58:09 -0500

    Committed Site

---
+++ PianoSite/app.js
@@ -0,0 +1,52 @@
+var main = function() {
+    var state;
+    //C button :D
+    $('#note-c').click(function() {
+        $('#c').trigger("play");
+    });
+    //C# button :D
+    $('#note-c#').click(function() {
+        $('#c#').trigger("play");
+    });
+    //D button :D
+    $('#note-d').click(function() {
+        $('#d').trigger("play");
+    });
+    //D# button :D
+    $('#note-d#').click(function() {
+        $('#d#').trigger("play");
+    });
+    //E button :D
+    $('#note-e').click(function() {
+        $('#e').trigger("play");
+    });
+    //F button :D
+    $('#note-f').click(function() {
+        $('#f').trigger("play");
+    });
+    //F# button :D
+    $('#note-f#').click(function() {
+        $('#f#').trigger("play");
+    });
+    //G button :D
+    $('#note-g').click(function() {
+        $('#g').trigger("play");
+    });
+    //G# button :D
+    $('#note-g#').click(function() {
+        $('#g#').trigger("play");
+    });
+    //A button :D
+    $('#note-a').click(function() {
+        $('#a').trigger("play");
+    });
+    //A#button :D
+    $('#note-a#').click(function() {
+        $('#a#').trigger("play");
+    });
+    //B button :D
+    $('#note-b').click(function() {
+        $('#b').trigger("play");
+    });
+}
+$(document).ready(main);
